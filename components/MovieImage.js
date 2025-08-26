b8ff7cae55fb952d1887e7e5d631cc2248c14328
musadek akhmad <musadekakhmad@gmail.com>
2025-08-26 14:16:45 +0700

    Update MovieImage.js

--- components/MovieImage.js
+++ components/MovieImage.js
@@ -1,6 +1,6 @@
 "use client";
 import React from 'react';
-import Image from 'next/image'; // Menggunakan komponen Image dari Next.js
+import Image from 'next/image'; // Use the Next.js Image component
 
 const MovieImage = ({ src, alt, className }) => {
   const handleError = (e) => {
@@ -15,8 +15,8 @@ const MovieImage = ({ src, alt, className }) => {
       src={src}
       alt={alt}
       className={className}
-      width={500} // Sesuaikan lebar yang sesuai
-      height={750} // Sesuaikan tinggi yang sesuai
+      width={500} // Adjust the appropriate width
+      height={750} // Adjust the appropriate height
       unoptimized={isPlaceholder}
       onError={handleError}
     />
